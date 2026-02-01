5835924d14e7b5a0c3401d3936f77b8e258ac5eb
Alexander K <alexander-kad@yandex.ru>
2026-02-01 19:35:42 +0500

    fixes

--- Sources/Autodoc/Classes/Managers/Router/RouterManagerImpl.swift
+++ Sources/Autodoc/Classes/Managers/Router/RouterManagerImpl.swift
@@ -4,6 +4,7 @@ import UIKit
 
 struct RouterManagerImpl: RouterManager {
     
+    @MainActor
     private var rootViewController: UINavigationController? {
         let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
         let window = windowScene?.windows.first { $0.isKeyWindow }
@@ -12,6 +13,13 @@ struct RouterManagerImpl: RouterManager {
     }
     
     func open(url: URL) {
+        Task {
+            await openSafari(url: url)
+        }
+    }
+    
+    @MainActor
+    private func openSafari(url: URL) {
         let viewController = SFSafariViewController(url: url)
         rootViewController?.pushViewController(viewController, animated: true)
     }
